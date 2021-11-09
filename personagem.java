1b852a0e7c8bd1191d45191424b6fa85aaf4a3f0
raphazg2 <90878707+raphazg2@users.noreply.github.com>
2021-11-08 21:35:03 -0300

    Create personagem.java

---
+++ personagem.java
@@ -0,0 +1,53 @@
+//CLASSE DO PERSONAGEM, COM SEUS ATRIBUTOS, METODOS E GETTERS/SETTERS
+public class Personagem {
+	
+	//ATRIBUTOS
+	private String nome;
+	private int ataque;
+	private int defesa;
+	private int pontosVida;
+	
+	//CONSTRUTOR
+	public Personagem(int ataque, int defesa, int pontosVida, String nome) {
+		this.setAtaque(ataque);
+		this.setDefesa(defesa);
+		this.setPontosVida(pontosVida);
+		this.setNome(nome);
+	}
+
+	public int getPontosVida() {
+		return pontosVida;
+	}
+	
+	public void setPontosVida(int pontosVida) {
+		this.pontosVida = pontosVida;
+	}
+	
+	public int getDefesa() {
+		return defesa;
+	}
+	
+	public void setDefesa(int defesa) {
+		this.defesa = defesa;
+	}
+	
+	public int getAtaque() {
+		return ataque;
+	}
+	
+	public void setAtaque(int ataque) {
+		this.ataque = ataque;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int atacar(){
+		return 0;
+	}
+}
