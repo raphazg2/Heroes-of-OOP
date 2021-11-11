b9eebf04f37d14070a96879745226d380d912c2b
raphazg2 <90878707+raphazg2@users.noreply.github.com>
2021-11-11 19:30:39 -0300

    Create Engine.java

---
+++ Engine.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Engine {
+	//LISTA USADA PARA GUARDAR OS PERSONAGENS CRIADOS PELO JOGADOR
+	List<Personagem> personagens = new ArrayList<Personagem>();
+	
+	//AQUI FOI INSTANCIADO(CRIADO) UM OBJETO DRAGÃO, QUAL SERA O NOSSO INIMIGO DO JOGO
+	Dragao dragao = new Dragao(10, 30, 300, "DRAGAO");
+	
+	public Engine(){}
+	
+	//METODO USADO PARA ADICIONAR UM PERSONAGEM QUE O JOGADOR CRIOU, NA LISTA DOS PERSONAGENS
+	public void addPersonagem(Personagem personagem) {
+		this.personagens.add(personagem);
+	}
+	
+	//METODO USADO APENAS PARA PEGAR TODOS OS PERSONAGENS DA LISTA E ESCREVER OS SEUS ATRIBUTOS NA TELA
+	public void escrevePersonagens() {
+		for(Personagem personagemList : personagens) {
+			personagemList.toString();
+			System.out.println("\n");
+		}
+	}
+	
+	//METODO A SER DESENVOLVIDO PARA INICIAR O JOGO
+	public void startGame() {
+		Random random = new Random();
+	    Scanner teclado = new Scanner(System.in);
+	    int turno = 0;
+		while(dragao.getPontosVida()>=0 && (personagens.size()!=0)){
+			turno ++;
+			for(Personagem personagemList : personagens) {
+				boolean ataca;
+				if(personagemList.getPontosVida() > 0) {
+					System.out.println("1-atacar");
+					System.out.println("2-defender");
+					System.out.println("\n"+personagemList.getNome()+" ESCOLHA UMA AÇÃO: ");
+					int escolha = teclado.nextInt();
+					while (escolha != 1 && escolha !=2) {
+						System.out.println("Valor invalido, digite um valor solicitado");
+						escolha = teclado.nextInt();
+					}
+					ataca = (escolha == 1) ? true : false;
+					if(ataca) {
+						dragao.setPontosVida(dragao.getPontosVida()-(personagemList.atacar()-dragao.getDefesa()));
+					}else {
+						//Implementar defesa personagem
+					}
+				}
+			}
+				if(dragao.getPontosVida()>0) {
+					int personagemAtacado = random.nextInt(personagens.size());
+					personagens.get(personagemAtacado).setPontosVida(personagens.get(personagemAtacado).getPontosVida()-dragao.getAtaque());
+					System.out.println(personagens.get(personagemAtacado).getNome() + " foi atacado, vida resultante: " + personagens.get(personagemAtacado).getPontosVida());
+					if(personagens.get(personagemAtacado).getPontosVida()<=0) {
+						System.out.println(personagens.get(personagemAtacado).getNome() + " morreu, seu corpo foi retirado do campo de batalha");
+						personagens.remove(personagemAtacado);
+					}
+					System.out.println("vida do Dragão após turno " + turno + ": " + dragao.getPontosVida());
+				}else {
+					System.out.println("PARABENS VOCE MATOU O DRAGÃO !");
+				}
+		}
+		if(dragao.getPontosVida()>0) {
+			System.out.println("VOCE FOI DERROTADO PELO DRAGAO !");
+		}
+	}
+}
+
